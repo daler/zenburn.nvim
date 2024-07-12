b7798adf304f82622f0b09593c8f0b3dcc09e66a
Ryan Dale <ryan.dale@nih.gov>
2024-07-11 20:10:06 -0400

    headline and codeblock colors

--- lua/zenburn/highlights/headlines.lua
+++ lua/zenburn/highlights/headlines.lua
@@ -1,6 +1,6 @@
 local c = require("zenburn.palette")
 return {
-  -- Headline = { bg=c.Search.bg },
-  -- CodeBlock = { bg="#3f3f3f"},
+  Headline = { bg=c.DiffChange.bg },
+  CodeBlock = { bg="#3f3f3f"},
 }
 
